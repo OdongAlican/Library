59ac2bf4539061e16c7fb7cdeb015d5121b8da29
OdongAlican <sandieo.2020@gmail.com>
2020-07-28 20:36:41 +0300

    update README file

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Library
-Mastering objects and object constructors in Javascript
+
+A simple book library app created with javascript
+
+## Built With
+
+- HTML,
+- CSS,
+- JavaScript
+- Bootstrap
+
+## live demo
+
+[live link](https://raw.githack.com/evansinho/Library/library/index.html)
+
+![Create Book](images/library_project.png)
+
+### Prerequisites
+
+- Any kind of browser supporting CSS3 and HTML5
+
+### Setup
+
+- Fork the repo to your remote repository.
+- Clone or download the repository to your local machine.
+
+### Install
+
+- No istallation required.
+
+
+## Authors
+
+👤 **Rahma Halane**
+
+- [Portfolio](https://raw.githack.com/imahnama/my-portfolio/develop/index.html)
+- [Twitter](https://twitter.com/halane_rahma)
+- [Linkedin](https://www.linkedin.com/in/rahmahalane/)
+- [Email](mailto:Halane.rahma@gmail.com )
+
+👤 **Odong Sunday**
+
+- [Portfolio](https://odongsunday.netlify.app/)
+- Github: [@OdongAlican](https://github.com/OdongAlican)
+- Twitter: [@odongsandie](https://twitter.com/odongsandie)
+- Linkedin: [Sunday Alican odong](https://www.linkedin.com/in/sunday-alican-odong/)
+- [Email](mailto:sandieo.2020@gmail.com)
+
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/OdongAlican/Library/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
--- main.js
+++ main.js
@@ -1,36 +1,20 @@
-let myLibrary = JSON.parse(localStorage.getItem('book-library-data')) || []; 
- const saveData = (arr) => { 
-   localStorage.setItem('book-library-data', JSON.stringify(arr)); 
- }; 
+const myLibrary = JSON.parse(localStorage.getItem('book-library-data')) || [];
 
-document.addEventListener("DOMContentLoaded", render)
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+const saveData = (arr) => {
+  localStorage.setItem('book-library-data', JSON.stringify(arr));
+};
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-
-  const title = document.getElementById('book-title').value
-  const author = document.getElementById('book-author').value
-  const pages = document.getElementById('book-pages').value
-  const read = document.querySelector('input[name="read"]:checked').value
-
-  const newBook = new Book(title,author,pages,read)
-
-  myLibrary.push(newBook)
-
-  saveData(myLibrary)
-
-  render()
-}
-
-function render(){
-    let output = "";
-    for(let i=0; i < myLibrary.length; i++){
-        output += `
+function render() {
+  let output = '';
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    output += `
       <div class="card col-lg-5 m-3 p-0">
          <div class="card-header w-100">
             No of Pages ${myLibrary[i].pages}
@@ -43,7 +27,7 @@ function render(){
               <p> <span class="title-head">Read Status:</span>
                 ${myLibrary[i].read}
               </p>
-              <footer class="blockquote-footer">Written By<cite title="Source Title">${myLibrary[i].author}</cite></footer>
+              <footer class="blockquote-footer">Written By <cite title="Source Title">${myLibrary[i].author}</cite></footer>
          </blockquote>
         </div>
         <div>
@@ -52,40 +36,52 @@ function render(){
         </div>
 
      </div>
-        `
-    }
+        `;
+  }
 
-    document.getElementById("book-list").innerHTML = output
+  document.getElementById('book-list').innerHTML = output;
 }
 
-document.getElementById("new-book").addEventListener("click", displayModal)
-const modal = document.getElementById('book-modal')
+function addBookToLibrary() {
+  const title = document.getElementById('book-title').value;
+  const author = document.getElementById('book-author').value;
+  const pages = document.getElementById('book-pages').value;
+  const read = document.querySelector('input[name="read"]:checked').value;
 
-function displayModal(){
+  const newBook = new Book(title, author, pages, read);
 
-  modal.classList.toggle('book-div')
+  myLibrary.push(newBook);
 
+  saveData(myLibrary);
+
+  render();
 }
+
+document.getElementById('add-book').addEventListener('click', addBookToLibrary);
+const modal = document.getElementById('book-modal');
+function displayModal() {
+  modal.classList.toggle('book-div');
+}
+
+document.getElementById('new-book').addEventListener('click', displayModal);
 
 function changeStatus(index) {
-     let book = myLibrary[index]
-     if (book.read == 'yes'){
-          book.read = 'no'
-          localStorage['book-library-data'] = JSON.stringify(myLibrary)
-          render()
-     }
-
-     else if (book.read == 'no') {
-          book.read = 'yes'
-          localStorage['book-library-data'] = JSON.stringify(myLibrary)
-          render()
-     }
-
+  const book = myLibrary[index];
+  if (book.read === 'yes') {
+    book.read = 'no';
+    localStorage['book-library-data'] = JSON.stringify(myLibrary);
+    render();
+  } else if (book.read === 'no') {
+    book.read = 'yes';
+    localStorage['book-library-data'] = JSON.stringify(myLibrary);
+    render();
+  }
 }
 
-
-function deleteBook(index){
-  myLibrary.splice(index, 1)
-  localStorage['book-library-data'] = JSON.stringify(myLibrary)
-  render()
+function deleteBook(index) {
+  myLibrary.splice(index, 1);
+  localStorage['book-library-data'] = JSON.stringify(myLibrary);
+  render();
 }
+
+document.addEventListener('DOMContentLoaded', render);
